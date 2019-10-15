481ffa2c9e66c353b0c343af1b7efad3ef2898f3
Kunal Basu <kunal.basu@berkeley.edu>
2019-10-14 22:31:03 -0700

    Checking

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,10 +17,6 @@ class MoviesController < ApplicationController
     if params[:sort].nil? && params[:ratings].nil? && session[:sort].nil? && session[:ratings].nil?
       return @movies
     end
-
-    if params[:sort].nil? && params[:ratings].nil? && (!session[:sort].nil? || !session[:ratings].nil?)
-      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
-    end
     
     @ratings_selected = params[:ratings]
     if !@ratings_selected.nil?
